fac8f7abefa78eefed05cf04241939eb06888ce0
goldbishop <goldbishop@DESKTOP-FIUD1NP>
2023-06-06 22:19:23 -0500

    Add project files.

---
+++ App/Output.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App {
+	public static class Output {
+
+		public static void Line (string msg ) {
+			Message ( msg + '\n' );
+		}
+
+		public static void Message ( string msg ) {
+			Console.Write ( msg );
+		}
+
+	}
+}
---
+++ App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App {
+    internal class Program {
+        static void Main(string[] args) {
+            Output.Line("Enter Input: ");
+            var inp = Console.ReadLine();
+            int WordCnt, CharCnt, NumCnt, SpecCnt;
+            /********************** DO NOT EDIT ABOVE THIS LINE **********************************/
+
+
+
+            // THIS SHOULD BE THE LAST STATEMENT FOR MAIN
+            Console.Read();
+        }
+
+    }
+}
